15c2ab5127fb302c552d947c6d48238d55ab0cc9
wyj965011173 <965011173@qq.com>
2016-10-31 20:30:59 +0800

    update 04

--- src/com/atguigu/git/test/TestGitHub.java
+++ src/com/atguigu/git/test/TestGitHub.java
@@ -3,5 +3,6 @@ package com.atguigu.git.test;
 public class TestGitHub {
 	public static void main(String[] args) {
 		System.out.println("hello github服务器01");
+		System.out.println("hello github服务器02");
 	}
 }

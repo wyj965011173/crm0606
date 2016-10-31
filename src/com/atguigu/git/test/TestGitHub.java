7e8c72bb32c324afe62a11736f29b0ec58536012
wyj965011173 <965011173@qq.com>
2016-10-31 20:35:08 +0800

    22
    22222

--- src/com/atguigu/git/test/TestGitHub.java
+++ src/com/atguigu/git/test/TestGitHub.java
@@ -4,5 +4,6 @@ public class TestGitHub {
 	public static void main(String[] args) {
 		System.out.println("hello github服务器01");
 		System.out.println("hello github服务器02");
+		System.out.println("p1,hello");
 	}
 }
